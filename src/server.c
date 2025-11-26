02fd2ed6645df4bdd376f69ef03b8e8773f6d4eb
bladsvytro <blads.vytro@gmail.com>
2025-11-26 21:06:20 +0300

    get app-server который асинхронно обрабатывает клиентские подключения по протоколам TCP и UDP с помощью механизма мультиплексирования (на базе epoll)

--- src/server.c
+++ src/server.c
@@ -1,38 +1,110 @@
 #include <arpa/inet.h> //htonl,  htons,  ntohl,  ntohs
 #include <netinet/in.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
+#include <sys/epoll.h>
 #include <sys/socket.h> //socket, listen, acept
 #include <sys/types.h>  //
 #include <unistd.h>     //
 #include "errproc.h"
 
+#define port 34521
+#define MAX_EVENTS 111
+#define BUFFER_SIZE 1024
+
 int main() {
-  int server = Socket(AF_INET, SOCK_STREAM, 0); // protocol TCP IpV4
-  struct sockaddr_in adr = {
-      0}; // vse polya proinitsializirovani 0 "0.0.0.0" - all ip to lisen
+    int epoll_fd;//try))))
+   int server = Socket(AF_INET, SOCK_STREAM, 0);
+  struct epoll_event event, events[MAX_EVENTS];
+  struct sockaddr_in adr = {0}; // vse polya proinitsializirovani 0 "0.0.0.0" - all ip to lisen
   adr.sin_family = AF_INET;
-  adr.sin_port = htons(34521); // host to network short
+  adr.sin_port = htons(port); 
   adr.sin_addr.s_addr = htonl(INADDR_ANY);
   Bind(server, &adr, sizeof adr);
-  Listen(server, 5); // que max 5
-  printf("Server listening on port 34521...\n");
-  struct sockaddr_in client_addr;
-  socklen_t client_len = sizeof(client_addr);
-  int fd = Accept(server, (struct sockaddr *)&client_addr, &client_len);
-  printf("Client connected!\n");
-  ssize_t nread;
-  char buf[256];
-  nread = read(fd, buf, 255);
-  if (nread == -1) {
-    perror("read failed");
-    exit(EXIT_FAILURE);
-  }
-  if (nread == 0) {
-    printf("END OF FILE\n");
-  }
-  write(STDOUT_FILENO, buf, nread);
-  sleep(15);
-  close(fd);
-  close(server);
+  Listen(server, MAX_EVENTS); // que max 5->111
+  printf("Server listening on port %d ...\n", port);
+  epoll_fd = Epoll_create1(0);
+  event.events =EPOLLIN;
+  event.data.fd =server;
+  printf("Epoll initialized. Waiting......\n");
+  while (1) {
+        // ЖДЕМ СОБЫТИЙ ОТ EPOLL (блокирующий вызов)
+        int nfds = Epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
+        printf("Received %d events\n", nfds);
+
+        // ОБРАБАТЫВАЕМ КАЖДОЕ СОБЫТИЕ
+        for (int i = 0; i < nfds; i++) {
+            // ЕСЛИ СОБЫТИЕ НА СЕРВЕРНОМ СОКЕТЕ - ЭТО НОВОЕ ПОДКЛЮЧЕНИЕ
+            if (events[i].data.fd == server) {
+                struct sockaddr_in client_addr;
+                socklen_t client_len = sizeof(client_addr);
+                
+                // ПРИНИМАЕМ НОВОЕ ПОДКЛЮЧЕНИЕ
+                int client_fd = Accept(server, (struct sockaddr*)&client_addr, &client_len);
+                printf("New client connected! fd=%d\n", client_fd);
+
+                // ДОБАВЛЯЕМ НОВОГО КЛИЕНТА В EPOLL ДЛЯ ОТСЛЕЖИВАНИЯ ЕГО ДАННЫХ
+                event.events = EPOLLIN;
+                event.data.fd = client_fd;
+                Epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_fd, &event);
+                char* welcome = "Подключение к серверу успешно!\n\n"
+                                "Вы можете:\n"
+                                "• Использовать команды через '/'\n\n"
+                                "Используйте \"/help\" для отображения списка команд\n"
+                                "Используйте \"/exit\" чтобы выйти\n\n";
+                write(client_fd, welcome, strlen(welcome));
+            } 
+            else {
+
+                int client_fd = events[i].data.fd;
+                char buf[BUFFER_SIZE];
+                
+                ssize_t nread = read(client_fd, buf, BUFFER_SIZE - 1);
+                
+                if (nread > 0) {
+                    buf[nread] = '\0';
+                    printf("Received from client %d: %s", client_fd, buf);
+
+                    write(client_fd, buf, nread);
+                    
+                } else if (nread == 0) {
+                    printf("Client %d disconnected\n", client_fd);
+                    close(client_fd);
+                } else {
+
+                    perror("read");
+                    close(client_fd);
+                }
+            }
+        }
+    }
+
+    close(server);
+    close(epoll_fd);
+    return 0;
 }
+
+
+
+
+
+
+//   struct sockaddr_in client_addr;
+//   socklen_t client_len = sizeof(client_addr);
+//   int fd = Accept(server, (struct sockaddr *)&client_addr, &client_len);
+//   printf("Client connected!\n");
+//   ssize_t nread;
+//   char buf[256];
+//   nread = read(fd, buf, 255);
+//   if (nread == -1) {
+//     perror("read failed");
+//     exit(EXIT_FAILURE);
+//   }
+//   if (nread == 0) {
+//     printf("END OF FILE\n");
+//   }
+//   write(STDOUT_FILENO, buf, nread);
+//   sleep(15);
+//   close(fd);
+//   close(server);
+// }
