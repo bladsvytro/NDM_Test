8525701809aabc8bfde3c3beb943550691d7cb0c
bladsvytro <blads.vytro@gmail.com>
2025-11-26 21:22:07 +0300

    clang-format ))) estick

--- src/server.c
+++ src/server.c
@@ -1,3 +1,4 @@
+#include "errproc.h"
 #include <arpa/inet.h> //htonl,  htons,  ntohl,  ntohs
 #include <netinet/in.h>
 #include <stdio.h>
@@ -6,7 +7,6 @@
 #include <sys/socket.h> //socket, listen, acept
 #include <sys/types.h>  //
 #include <unistd.h>     //
-#include "errproc.h"
 
 #define port 34521
 #define MAX_EVENTS 111
@@ -14,79 +14,78 @@
 
 int main() {
   int count_users = 0;
-    int epoll_fd;//try))))
-   int server = Socket(AF_INET, SOCK_STREAM, 0);
+  int epoll_fd; // try))))
+  int server = Socket(AF_INET, SOCK_STREAM, 0);
   struct epoll_event event, events[MAX_EVENTS];
-  struct sockaddr_in adr = {0}; // vse polya proinitsializirovani 0 "0.0.0.0" - all ip to lisen
+  struct sockaddr_in adr = {
+      0}; // vse polya proinitsializirovani 0 "0.0.0.0" - all ip to lisen
   adr.sin_family = AF_INET;
-  adr.sin_port = htons(port); 
+  adr.sin_port = htons(port);
   adr.sin_addr.s_addr = htonl(INADDR_ANY);
   Bind(server, &adr, sizeof adr);
   Listen(server, MAX_EVENTS); // que max 5->111
   printf("Server listening on port %d ...\n", port);
   epoll_fd = Epoll_create1(0);
-  event.events =EPOLLIN;
-  event.data.fd =server;
+  event.events = EPOLLIN;
+  event.data.fd = server;
   printf("Epoll initialized. Waiting......\n");
   while (1) {
-        int nfds = Epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
-        printf("Received %d events\n", nfds);
-        for (int i = 0; i < nfds; i++) {
- 
-            if (events[i].data.fd == server) {
-                struct sockaddr_in client_addr;
-                socklen_t client_len = sizeof(client_addr);
+    int nfds = Epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
+    printf("Received %d events\n", nfds);
+    for (int i = 0; i < nfds; i++) {
 
-                int client_fd = Accept(server, (struct sockaddr*)&client_addr, &client_len);
-                count_users++;
-                printf("New client connected! fd=%d, Online:%d\n", client_fd,count_users);
-                event.events = EPOLLIN;
-                event.data.fd = client_fd;
-                Epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_fd, &event);
-                char* welcome = "Подключение к серверу успешно!\n\n"
-                                "Вы можете:\n"
-                                "• Использовать команды через '/'\n\n"
-                                "Используйте \"/help\" для отображения списка команд\n"
-                                "Используйте \"/exit\" чтобы выйти\n\n";
-                write(client_fd, welcome, strlen(welcome));
-                
-            } 
-            else {
+      if (events[i].data.fd == server) {
+        struct sockaddr_in client_addr;
+        socklen_t client_len = sizeof(client_addr);
 
-                int client_fd = events[i].data.fd;
-                char buf[BUFFER_SIZE];
-                
-                ssize_t nread = read(client_fd, buf, BUFFER_SIZE - 1);
-                
-                if (nread > 0) {
-                    buf[nread] = '\0';
-                    printf("Received from client %d: %s", client_fd, buf);
+        int client_fd =
+            Accept(server, (struct sockaddr *)&client_addr, &client_len);
+        count_users++;
+        printf("New client connected! fd=%d, Online:%d\n", client_fd,
+               count_users);
+        event.events = EPOLLIN;
+        event.data.fd = client_fd;
+        Epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_fd, &event);
+        char *welcome = "Подключение к серверу успешно!\n\n"
+                        "Вы можете:\n"
+                        "• Использовать команды через '/'\n\n"
+                        "Используйте \"/help\" для отображения списка команд\n"
+                        "Используйте \"/exit\" чтобы выйти\n\n";
+        write(client_fd, welcome, strlen(welcome));
 
-                    write(client_fd, buf, nread);
-                    
-                } else if (nread == 0) {
-                    printf("Client %d disconnected  online: %d \n", client_fd, count_users-1);
-                    close(client_fd);
-                    count_users--;
-                } else {
-                    perror("read");
-                    close(client_fd);
-                    printf("Client %d disconnected  online: %d \n", client_fd, count_users-1);
-                    count_users--;
-                }
-            }
+      } else {
+
+        int client_fd = events[i].data.fd;
+        char buf[BUFFER_SIZE];
+
+        ssize_t nread = read(client_fd, buf, BUFFER_SIZE - 1);
+
+        if (nread > 0) {
+          buf[nread] = '\0';
+          printf("Received from client %d: %s", client_fd, buf);
+
+          write(client_fd, buf, nread);
+
+        } else if (nread == 0) {
+          printf("Client %d disconnected  online: %d \n", client_fd,
+                 count_users - 1);
+          close(client_fd);
+          count_users--;
+        } else {
+          perror("read");
+          close(client_fd);
+          printf("Client %d disconnected  online: %d \n", client_fd,
+                 count_users - 1);
+          count_users--;
         }
+      }
     }
+  }
 
-    close(server);
-    close(epoll_fd);
-    return 0;
+  close(server);
+  close(epoll_fd);
+  return 0;
 }
-
-
-
-
-
 
 //   struct sockaddr_in client_addr;
 //   socklen_t client_len = sizeof(client_addr);
